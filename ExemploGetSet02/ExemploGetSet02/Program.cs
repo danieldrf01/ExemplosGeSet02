d860f6b33adb2e0845eb1480d253422de3a80968
Marcos Luiz Mittag Filho <marquinhoszx_xd@outlook.com>
2018-06-05 17:28:24 -0300

    ...

---
+++ ExemploGetSet02/ExemploGetSet02/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploGetSet02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cabelo cabelo = new Cabelo();
+
+            cabelo.SetTamanhoDoFio("a");
+            cabelo.SetGrossuraDoFio(0.1);
+            cabelo.SetCorDoFio(""""""");
+
+            Console.WriteLine(cabelo.GetTamanhoDoFio());
+            Console.WriteLine(cabelo.);
+
+        }
+    }
+}
